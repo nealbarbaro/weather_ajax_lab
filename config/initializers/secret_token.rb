1c350611bed0c4d80cef8fc6e6e9d9702cb7a57e
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-03 15:02:53 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherAjaxLab::Application.config.secret_token = 'f26e2fae886fa695201005e001de1c8452eb03c1e703278d2545fb234c9d1c42dd74ae6a72bfdf217de3a1eccac7574e6ca3c0e6268deca6dc05a964191ffb51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WeatherAjaxLab::Application.config.session_store :cookie_store, key: '_weather_ajax_lab_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WeatherAjaxLab::Application.config.session_store :active_record_store
